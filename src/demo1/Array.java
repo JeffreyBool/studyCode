3cf333ec4a19e2fffebac6874cc5a65b811c9beb
张高元 <zhanggaoyuan@singulato.com>
2018-10-18 15:10:44 +0800

    Java array 基本使用

---
+++ src/demo1/Array.java
@@ -0,0 +1,14 @@
+package demo1;
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+
+        int[] b;
+        b = new int[10];
+
+        int[] c = {1,2,3,4};
+        
+    }
+}
