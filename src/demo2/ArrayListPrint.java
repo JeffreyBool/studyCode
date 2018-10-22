69793487377b2ce818aaade19bc49ebd7c8349d7
张高元 <zhanggaoyuan@singulato.com>
2018-10-23 01:10:37 +0800

    ArrayList 自定输出方法

--- src/demo2/ArrayListPrint.java
+++ src/demo2/ArrayListPrint.java
@@ -11,5 +11,18 @@ public class ArrayListPrint {
         list.add("黑白无常");
 
         System.out.println(list);
+        printArrayList(list);
+    }
+
+    public static void printArrayList(ArrayList<String> list){
+        System.out.print("{");
+        for (int i = 0; i < list.size(); i++) {
+            String name = list.get(i);
+            if(i == list.size()-1){
+                System.out.println(name+ "}");
+            }else{
+                System.out.print(name + "@");
+            }
+        }
     }
 }
